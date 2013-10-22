67ad900ccebbcc93c0c4e02770e2bc433877d4ec
panjingwei1945 <panjingwei1945@hotmail.com>
2013-10-22 11:02:25 +0800

    test new version 2

---
+++ autoshutdown.bat
@@ -0,0 +1,22 @@
+@echo off 
+set addr=10.10.168.1
+@ping -n 1 -w 100 %addr% | find "TTL"
+IF errorlevel 1 (
+goto ping_again
+) ELSE (
+goto end
+)
+
+:ping_again
+@ping %addr% | find "TTL"
+IF errorlevel 1 (
+goto closecomputer
+) ELSE (
+goto end
+)
+
+:closecomputer
+shutdown  /s /t 120 /c "Fail to ping 10.10.168.1 ,computer will shutdown in 120s. Cancel command: shutdown /a"
+
+:end
+exit
